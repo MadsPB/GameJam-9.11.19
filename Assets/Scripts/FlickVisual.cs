9a1ccbd90e9969f8ac8a2065bff2fe51ac777bb4
Arda Cevik <cevikarda@gmail.com>
2019-11-09 16:49:29 +0100

    Flick, input manager and stuff.

---
+++ Assets/Scripts/FlickVisual.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class FlickVisual : MonoBehaviour
+{
+    public InputManager InputManager;
+    public List<GameObject> ArrowPieces;
+
+    private Vector2 _startingPointerPosition;
+
+    public void Start()
+    {
+        InputManager.PointerDown += HandlePointerDown;
+        InputManager.PointerUp += HandlePointerUp;
+        InputManager.Drag += HandleDrag;
+    }
+
+    private void HandlePointerDown(Vector2 position)
+    {
+        _startingPointerPosition = position;
+        Show();
+    }
+
+    private void HandlePointerUp(Vector2 position)
+    {
+        _startingPointerPosition = Vector2.zero;
+        Hide();
+    }
+
+    private void HandleDrag(Vector2 position)
+    {
+        var yScale = position - _startingPointerPosition;
+        var differenceVector = position - _startingPointerPosition;
+        var angle = Mathf.Atan2(differenceVector.y, differenceVector.x);
+
+        var localScale = transform.localScale;
+        localScale = new Vector3(localScale.x, yScale.magnitude * 0.01f, localScale.z);
+        transform.localScale = localScale;
+        transform.localRotation = Quaternion.Euler(0f, 0f, angle * 180f);
+    }
+
+    private void Show()
+    {
+        foreach (var arrowPiece in ArrowPieces)
+        {
+            arrowPiece.SetActive(true);
+        }
+    }
+
+    private void Hide()
+    {
+        foreach (var arrowPiece in ArrowPieces)
+        {
+            arrowPiece.SetActive(false);
+        }
+    }
+}
---
+++ Assets/Scripts/InputManager.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.EventSystems;
+using System;
+
+public class InputManager : MonoBehaviour, IPointerDownHandler, IDragHandler, IPointerUpHandler
+{
+    public event Action<Vector2> PointerDown;
+    public event Action<Vector2> PointerUp;
+    public event Action<Vector2> Drag;
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        PointerDown?.Invoke(eventData.position);
+    }
+
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        PointerUp?.Invoke(eventData.position);
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        Drag?.Invoke(eventData.position);
+    }
+}

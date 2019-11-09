ccc55f35884cc6d422460fecdbee6c706af849c8
Arda Cevik <cevikarda@gmail.com>
2019-11-09 20:27:34 +0100

    fix

--- Assets/Scripts/DialogueManager.cs
+++ Assets/Scripts/DialogueManager.cs
@@ -29,7 +29,7 @@ namespace DefaultNamespace
             }
             else
             {
-                GameManager.HandleDialogueFinished();
+//                GameManager.HandleDialogueFinished();
             }
         }
     }

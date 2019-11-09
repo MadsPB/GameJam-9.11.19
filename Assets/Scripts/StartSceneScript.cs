08a86f41d26dc42565365bb1ce88ab2ddde04cd9
Madspb <baadsmand@gmail.com>
2019-11-09 18:20:34 +0100

    add tmp and a start scene

---
+++ Assets/Scripts/StartSceneScript.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class StartSceneScript : MonoBehaviour
+{
+    public Button startButton;
+    // Start is called before the first frame update
+    void Start()
+    {
+        startButton.onClick.AddListener(StartGame);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void StartGame()
+    {
+        SceneManager.LoadScene(1);
+    }
+}
